d18d3b261f43d353c7d0ebf1ab922d40c9f63891
Neel Shah <neeldevenshah@gmail.com>
2024-04-01 23:29:49 +0530

    Added RAG code for text, remaining to re-configure for project

---
+++ Rag_VectorSearch_for_text/Tokenizer_Server/server.py
@@ -0,0 +1,32 @@
+import flask
+from flask import request, jsonify
+from sentence_transformers import SentenceTransformer
+import json
+
+
+app = flask.Flask(__name__)
+# Enable CORS
+from flask_cors import CORS
+CORS(app)
+@app.route("/checking", methods=["GET"])
+def checking():
+    return "Working!!"
+
+@app.route("/preprocess", methods=["POST"])
+def preprocess_text():
+    try:
+        data = request.get_json()
+        texts = ""
+        for key, value in data.items():
+            texts.join(f" {key}: {value}")
+        print("-----------------------")
+        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+        embeddings = model.encode(texts)
+        embeddings = embeddings.tolist()  # Convert ndarray to list
+        print("Processed a request successfully")
+        return jsonify({"embeddings": embeddings})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # Change port if needed
